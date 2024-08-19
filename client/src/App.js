6012a2aef722bb6130fe7ed7b5dcb762eb6dc209
TenHam3 <simbaandbella3@gmail.com>
2024-08-19 00:43:46 -0500

    Created landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
 import './App.css';
+import Sidebar from './components/sidebar';
+import Card from './components/card';
 
 function App() {
+  // Get user's username from backend and pass it into greeting in header
+
+  const name = "Documents";
+  const iconLink = "/images/file-document.svg"
   return (
-    <div>hello, world</div>
+    <div className="container">
+      <div className="header">
+        <div className="greeting">
+          <div className="pfp-container">
+            <img src="/images/cat.png" alt="Profile" className="contained-img" />
+          </div>
+          <h1>Hello, username</h1>
+        </div>
+        <h1 className="title">Collaborative Learning Platform</h1>
+      </div>
+      <Sidebar />
+      <div className="content">
+        <div className="cards-container">
+          <Card cardName={name} cardIcon={iconLink} />
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ client/src/components/sidebar.js
@@ -0,0 +1,28 @@
+import '../sidebar.css';
+
+export default function Sidebar() {
+    return (
+    <div className="sidebar">
+        {/* Make logo a button that returns to home page in future */}
+        <div className="logo-container"> 
+            <img src="/images/paperclip.svg" alt="Logo" className="contained-img" />
+        </div>
+        <nav className="navbar">
+            <ul className="nav-links">
+                <li className="nav-item">
+                    <div className="nav-link-container">
+                        <img className="icon" alt="Settings" src="/images/cog.svg" />
+                        <a className="nav-link">Settings</a>
+                    </div>
+                </li>
+                <li className="nav-item">
+                    <div className="nav-link-container">
+                        <img className="icon" alt="Account" src="/images/account-circle.svg" />
+                        <a className="nav-link">Account</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    );
+}
